9bf347e8a9530b10af88f9406781f5e0afffc909
Duc Anh Le <leducan1@fel.cvut.cz>
2020-04-09 11:27:37 +0200

    test app

---
+++ src/test/java/springboot/application/filmatory/FilmatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.application.filmatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmatoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
